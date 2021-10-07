a0e95ae3813017da7cd3068dca3f4d65539a9234
juanrocha <aguilajk@gmail.com>
2021-10-07 13:45:42 +0200

    first round of API requests (1-10k)

--- 02-retrieve_abstracts.R
+++ 02-retrieve_abstracts.R
@@ -7,6 +7,7 @@ library(tidyverse)
 library(fs)
 library(here)
 library(rscopus)
+library(tictoc)
 
 
 load("data/missing_abstracts.RData")
@@ -14,17 +15,41 @@ load("data/missing_abstracts.RData")
 
 res <- list() # response list
 
-res <- df_lack |> 
+dois <- df_lack |> 
     slice(1:10000) |> # API allows 10k per week, change index every week
-    pull(doi) |> 
-    abstract_retrieval(identifier = "doi") 
+    pull(doi)
+
+tic()
+for (i in seq_along(dois)){
+    res[[i]] <- abstract_retrieval(id = dois[i], identifier = "doi") 
+    Sys.sleep(0.2)
+}
+toc()
+
+## The res (response) object is over 8.7GB, not point of saving it all, only the
+## abstracts which are already >20MB for 1000 records. It does however return
+## full reference information. Good to know for future work.
+
+## abs_safe extract the abstract safely, if there was an error in the call, it returns null.
+abs_safe <- safely(
+    function(x){ 
+        x$content$`abstracts-retrieval-response`$item$bibrecord$head$abstracts 
+    }
+)
+
+
+abs_safe(res[[597]])
 
 abstracts <- res |> 
-    purrr::map_chr(function(x){ 
-        x$content$`abstracts-retrieval-response`$item$bibrecord$head$abstracts 
-    })
+    map(abs_safe)
+    
+abstracts[[597]] # an example of an error, it should be stored as NULL
 
+abstracts <- transpose(abstracts)
 
+df_1_10k <- tibble(
+    doi = dois,
+    abstract = abstracts$result) |> 
+    unnest(abstract, keep_empty = TRUE)
 
-
-
+save(df_1_10k, file = "data/abstracts_1_10k.RData")
